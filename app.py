b938dd1e9e8e5433426702582c494f16ce0c1594
VaibhaviChaudhari <vaibhavichaudhari.vc@gmail.com>
2023-05-28 17:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -217,18 +217,18 @@ def main():
 
                                 if st.form_submit_button("Predict and Send"):
                                     input_validation(patient_name, patient_contact_number, doctor_name, doctor_contact_number)
-                                    result = Skin_detect(result_img)
+                                    result = disease_detect(result_img)
                                     st.success("Whatsapp message sent successfully!")
-                                    if result[0][0]<0.50:
-                                        # class_ind = list(result).index(max_prob)
-                                        # class_name = classes[class_ind]
-                                        # short_name = class_name[0]
-                                        #full_name = classes[0]
-                                        print('Skin is diseased')
+                                    # if result[0][0]<0.50:
+                                    #     # class_ind = list(result).index(max_prob)
+                                    #     # class_name = classes[class_ind]
+                                    #     # short_name = class_name[0]
+                                    #     #full_name = classes[0]
+                                    #     print('Skin is diseased')
                                 
-                                    else:
-                                        #full_name = classes[1]
-                                        print('Skin is Healthy, No Disease detected')
+                                    # else:
+                                    #     #full_name = classes[1]
+                                    #     print('Skin is Healthy, No Disease detected')
 
                         
                     if st.checkbox('model1'):
